3370e09782895217483dc68af4558c15d2fe06b7
Svetlana Jugai <nyah88@gmail.com>
2016-11-05 13:30:05 +0200

    table

---
+++ src/parktikum5/Tabel.java
@@ -0,0 +1,19 @@
+package parktikum5;
+
+/**
+ * Created by svetlana on 5.11.16.
+ */
+public class Tabel {
+    public static void main(String[] args) {
+
+        int tabeliSuurus = 7;
+
+        for (int j = 0; j < tabeliSuurus; j++){
+            for (int i = 0; i < tabeliSuurus; i++) {
+                System.out.print("0 ");
+            }
+            System.out.println(); //reavahetus
+        }
+
+    }
+}
